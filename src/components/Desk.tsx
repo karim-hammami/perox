4c20e2bf40025887d1cb9acaed24692c190664ca
karim-hammami <hammamik790@gmail.com>
2023-05-31 01:59:37 +0100

    fix text on the description

--- src/components/Desk.tsx
+++ src/components/Desk.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 function Desk() {
   return (
     <div className="flex items-center m-0 justify-center h-screen py-10  w-full">
-      <h1 className="text-[#FFFF00] font-medium text-xs overflow-x-hidden md:overflow-hidden whitespace-normal lg:text-4xl px-4 mb-20 mt-28 md:p-20" >Skander Werzli A.k.a PEROX was born on January 5th 2001 in La Marsa, Tunis. <br></br>
+      <h1 className="text-[#FFFF00] font-medium text-sm overflow-x-hidden md:overflow-hidden whitespace-normal lg:text-4xl px-4 mb-20 mt-28 md:p-20" >Skander Werzli A.k.a PEROX was born on January 5th 2001 in La Marsa, Tunis. <br></br>
 PEROX began his career at the age of 13 by producing electronic music, in 2015 he entered music school to further his knowledge in music theory, in 2018 he began producing hip-hop music and started experimenting with mixing electronic music and hip-hop together to create a unique flavor and sound, in 2020 he signed with a Tunisian production company and began producing for a variety of mainstream artists in the Tunisian Hip-hop scene. 
 
-PEROX produces and publish his music on a variety of music platforms and social media sites, he also distribute music on digital music platforms under the name of his company which he founded in 2018 "PEROXANA RECORDS" that features other artists.
+<br></br>PEROX produces and publish his music on a variety of music platforms and social media sites, he also distribute music on digital music platforms under the name of his company which he founded in 2018 "PEROXANA RECORDS" that features other artists.
 
-PEROX has collaborated with many artists and music producers across the globe expanding his Discography and captivating millions of streams on Youtube and other platforms.</h1>
+<br></br>PEROX has collaborated with many artists and music producers across the globe expanding his Discography and captivating millions of streams on Youtube and other platforms.</h1>
     </div>
   )
 }
