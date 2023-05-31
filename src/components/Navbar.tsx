0b0c9b7b0c22e094d39d19525e05165378e126d1
karim-hammami <hammamik790@gmail.com>
2023-05-31 01:56:03 +0100

    begin to work on implementing the dropdown menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,55 @@
+"use client";
+import { useState, useEffect, useRef } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import Insta from './Insta';
 
 interface Props {
   className?: string;
 }
 
 const Navbar: React.FC<Props> = ({ className }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent | TouchEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        closeDropdown();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('touchstart', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('touchstart', handleClickOutside);
+    };
+  }, []);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const closeDropdown = () => {
+    setIsDropdownOpen(false);
+  };
+
   return (
-    <header
-      className={`fixed top-0 left-0 right-0 backdrop-blur-2xl  flex items-center z-50 justify-between bg-transparent py-2 md:py-5 px-6 ${className ?? ''}`}>
-      <a href="/" className=" flex items-center">
+    <header className={`fixed top-0 left-0 right-0 backdrop-blur-2xl flex items-center z-50 justify-between w-full py-2 md:py-5 px-6 ${className ?? ''}`}>
+      <Link href="/" className="flex items-center">
         <Image src='/LOGOS.png' alt='logo' width={60} height={20} />
-      </a>
-      <Image src={'/hamburger.svg'} alt="ham" width={60} height={20}/>
+      </Link>
+      <div className="relative">
+        <Image src={'/hamburger.svg'} alt="ham" width={30} height={30} className="cursor-pointer" onTouchStart={toggleDropdown} onClick={toggleDropdown} />
+        {isDropdownOpen && (
+          <div ref={dropdownRef} className="absolute top-full right-0 mt-2 bg-white shadow-lg rounded-md">
+            <Link href="/about" className="block px-4 py-2 text-gray-800 hover:bg-gray-200" onClick={closeDropdown}>About</Link>
+            <Link href="/services" className="block px-4 py-2 text-gray-800 hover:bg-gray-200" onClick={closeDropdown}>Services</Link>
+            <Link href="/contact" className="block px-4 py-2 text-gray-800 hover:bg-gray-200" onClick={closeDropdown}>Contact</Link>
+          </div>
+        )}
+      </div>
     </header>
   );
 };
