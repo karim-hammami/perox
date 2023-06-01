d436c538a80fe7eb051258c5cea772f829303d4d
karim-hammami <hammamik790@gmail.com>
2023-06-01 13:15:10 +0100

    working on sending emails

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,17 +1,41 @@
 import { NextRequest, NextResponse } from "next/server";
+import nodemailer from "nodemailer"
+
 
 interface em {
   email: string,
-  body: string,
+  message: string,
 }
 
 
-export async function POST(req: NextRequest) {
+export async function POST(req: Request, res: Response) {
     // Get the todo data from the request body
-    const todoData= req.body;
-    console.log("hit")
-    console.log(todoData)
+    const reso: em = await req.json()
+
+    
+
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: 'your-email@gmail.com',
+        pass: 'your-password',
+      },
+    });
+
+    const mailOptions = {
+      from: reso.email, // Use the email entered by the user
+      to: 'your-email@gmail.com', // Replace with your Gmail email address
+      subject: 'Email from Next.js Form',
+      text: reso.message,
+    };
+    try {
+      // Send the email
+      const info = await transporter.sendMail(mailOptions);
+      console.log('Email sent:', info.response);
+      return new NextResponse(JSON.stringify({"message": "message is sent!!!!"}))
+    } catch (error) {
+      console.error('Error sending email:', error);
+      return new NextResponse(JSON.stringify({"message": "message failed!!!"}))
+    }
   
-    // Create a new todo in the database
-    return new Response(todoData, {status: 200});
   }
