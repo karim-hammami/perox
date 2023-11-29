d7a01999d3cb396cc8922f1fa9e5394c0b5e582f
karim-hammami <hammamik790@gmail.com>
2023-11-29 14:59:49 +0100

    working on Navbar component

--- src/_components/global/Navbar.tsx
+++ src/_components/global/Navbar.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Image from 'next/image'
 import React, { useState } from 'react'
 
 export default function Navbar() {
@@ -6,6 +7,9 @@ export default function Navbar() {
     return (
         <>
             <div className='bg-background w-screen h-24'>
+                <div className='flex mx-48 items-center py-4'>
+                    <Image src="/logo.webp" alt="logo" width={50} height={50} />
+                </div>
             </div>
         </>
     )
