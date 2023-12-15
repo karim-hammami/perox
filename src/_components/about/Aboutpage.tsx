f3cc08ee55b2093ca95054eb202580c348e9efdc
karim-hammami <hammamik790@gmail.com>
2023-12-15 15:10:12 +0100

    feat: creating the about page components

---
+++ src/_components/about/Aboutpage.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import { Content } from "./Content";
+
+
+export const Aboutpage = () => {
+    return (
+        <>
+            <div className="bg-background w-full h-screen relative z-10">
+                <Content />
+            </div>
+        </>
+    )
+}
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { Aboutpage } from "@/_components/about/Aboutpage";
 import Navbar from "@/_components/global/Navbar";
 
 export default function About() {
     return (
         <main>
             <Navbar />
+            <Aboutpage />
         </main>
     )
 }
